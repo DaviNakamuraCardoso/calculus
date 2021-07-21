bc8d8a6cee8d1ef76dc8ff5b842bc390942223ba
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-07-21 20:35:24 -0300

    More integrals

--- chapter_04/integrals.c
+++ chapter_04/integrals.c
@@ -7,6 +7,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 
@@ -30,7 +31,7 @@ ld integral(ld (*f) (ld), ld n, ld start, ld end, enum side s)
 
     for (step = ((end-start) / n), val = start, i = 0; i < n; i++, val += step)
     {
-        sum += f(val);    
+        sum += (f(val) + f(val+step)) / 2;    
     }    
     
     return sum * step;
@@ -42,16 +43,41 @@ static ld f1(ld x)
     return powl(x, 4);
 }
 
+static ld f2(ld x)
+{
+    return powl(x, 2) - 4; 
+}
+
+static ld f3(ld x)
+{
+    return sinl(sqrtl(x)); 
+}
+
+static ld f4(ld x)
+{
+    return (x / (x+1)); 
+}
+
+static ld f5(ld x)
+{
+    return x*x; 
+
+}
+
+static ld f6(ld x)
+{
+    return sinl(5*x); 
+}
 
 int main(void)
 {
     ld n[] = {
-        10, 30, 50, 100, 1000, 10000
+        5, 10, 50, 100, 1000, 10000
     }; 
 
     for (int i = 0; i < 6; i++)
     {
-        printf("The approximation for %.0Lf rectangles is %Lf\n", n[i], integral(cosl, n[i], 0, PI/2, RIGHT));  
+        printf("The approximation for %.0Lf rectangles is %Lf\n", n[i], integral(f6, n[i], 0, PI,  RIGHT));  
     
     }
 
