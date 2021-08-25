c78197170d5ba970f12aeba6a9c378486d13562a
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-08-24 22:10:52 -0300

    Euler's Rule implementation in C

---
+++ chapter_08/euler.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+typedef long double ld; 
+
+#define e 2.1718281828459045
+
+ld euler(ld initial, ld (*f) (ld,ld), ld step, ld start, ld end)
+{
+    ld y = initial;
+
+    for (ld x = start; x < end; x+=step)
+    {
+        y += f(x,y) * step;
+    }
+
+    return y;
+}
+        
+
+ld f1(ld x, ld y)
+{
+    return y - 2 * x;
+}
+
+ld f2(ld x, ld y)
+{
+    return y + x * y;
+}
+
+ld f3(ld x, ld y)
+{
+    return 6 * powl(x,2) - 3 * powl(x,2) * y;
+}
+
+int main(void)
+{
+    ld real, ap, error; 
+    ld steps[] = { 1, 0.1, 0.01, 0.001 }; 
+
+    //printf("%Lf\n", euler(0, f1, 0.5, 1, 3));
+    //printf("%Lf\n", euler(1, f2, 0.1, 0, 0.5)); 
+
+
+
+    real = 2 + powl(e, -1);
+
+    for (short i = 0; i < 4; i++)
+    {
+        ap = euler(3, f3, steps[i], 0, 1); 
+        error = real - ap;
+        printf("The approximation for step %Lf is %Lf\n", steps[i], ap); 
+        printf("The error in this approximation is %Lf\n\n", (error > 0)? error : -error); 
+    }
+
+
+    inline ld f4(ld x, ld y) { return 12 - 4 * y; }
+
+    printf("%Lf\n", euler(0, f4, 0.1, 0, 0.5));
+
+
+    return 0; 
+}
+
+
+
+
--- chapter_08/integrals.m
+++ chapter_08/integrals.m
@@ -24,6 +24,8 @@ function y = l (x)
     y = 2 * pi * (x + x^3) * sqrt(1 + (1 + 3*x^2)^2);
 endfunction
 
+
+
 [area,ierr,nfun,err] = quad("f",0,3.14/3);
 [area2,ierr,nfun,err] = quad("g",0,3.14/3);
 [area3,ierr,nfun,err] = quad("h",-1,1);
@@ -32,6 +34,15 @@ endfunction
 [area6,ierr,nfun,err] = quad("l",0,1);
 [area7,ierr,nfun,err] = quad(@(x) (2*pi*x*sqrt(1+9*x^4)), 0,1);
 
+% Garbage Project
+[area8,ierr,nfun,err] = quad(@(x) (e^((-(x-9.4)^2)/(2*4.2^2)) / (4.2*sqrt(2*pi))), 10,Inf);
+
+% Speed Limit
+[area9,ierr,nfun,err] = quad(@(x) (e^((-(x-112)^2)/(2*8^2)) / (8*sqrt(2*pi))), 0,100);
+[area10,ierr,nfun,err] = quad(@(x) (e^((-(x-112)^2)/(2*8^2)) / (8*sqrt(2*pi))), 125,Inf);
+
+
+
 printf("%f\n",area); 
 printf("%f\n",area2); 
 printf("%f\n",area3); 
@@ -39,5 +50,8 @@ printf("%f\n",area4);
 printf("%f\n",area5); 
 printf("%f\n",area6); 
 printf("%f\n",area7); 
+printf("%f\n",area8); 
+printf("The probability of a vehicle traveling in legal speed is %f\n",area9); 
+printf("The probability of getting a ticket is %f\n",area10); 
 
 
